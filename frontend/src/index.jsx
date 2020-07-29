731e3cff4ecb69ec811aba079d6b45a9e5acda81
Victor <kuro_caelum@outlook.com>
2020-07-29 12:08:23 -0300

    Integração com Redux e com backend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
 
-import App from './main/app'
+import promise from "redux-promise";
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import reducers from "./main/reducers";
+import App from "./main/app";
+
+const store = applyMiddleware(promise)(createStore)(reducers);
+
+ReactDOM.render(
+   <Provider store={store}>
+      <App />
+   </Provider>,
+   document.getElementById("app")
+);
