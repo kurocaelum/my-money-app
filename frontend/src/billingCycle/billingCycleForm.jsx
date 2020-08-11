232a8569b22e2460b3339003382fb98effcacbf9
Victor <kuro_caelum@outlook.com>
2020-08-11 00:42:11 -0300

    Sumário nos formulários

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,10 +6,20 @@ import { bindActionCreators } from 'redux'
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
 import ItemList from './itemList'
+import Summary from './summary'
 
 class BillingCycleForm extends Component {
+   calculateSummary() {
+      const sum = (t, v) => t + v
+      return {
+         sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+         sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum),
+      }
+   }
+
    render() {
       const { handleSubmit, readOnly, credits, debts } = this.props
+      const { sumOfCredits, sumOfDebts } = this.calculateSummary()
 
       return (
          <form role='form' onSubmit={handleSubmit}>
@@ -40,6 +50,7 @@ class BillingCycleForm extends Component {
                   cols='12 4'
                   placeholder='Informe o ano'
                />
+               <Summary credit={sumOfCredits} debt={sumOfDebts} />
                <ItemList
                   cols='12 6'
                   list={credits}
@@ -53,7 +64,7 @@ class BillingCycleForm extends Component {
                   readOnly={readOnly}
                   field='debts'
                   legend='Débitos'
-                  showStatus
+                  showStatus={true}
                />
             </div>
             <div className='box-footer'>
