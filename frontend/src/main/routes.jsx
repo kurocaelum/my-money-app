8baa0b6f776456beb2fa1d917b4d6c6fe92f72e6
Victor <kuro_caelum@outlook.com>
2020-08-16 05:55:57 -0300

    Nova funcionalidade de login; Refatoramento para navegação com React Router;

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
+import AuthOrApp from './authOrApp'
 import Dashboard from '../dashboard/dashboard'
 // import Dashboard from '../dashboard2/dashboard2'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
-    <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
-        <Route path='/billingCycles' component={BillingCycle} />
-        <Redirect from='*' to='/' />
-    </Router>
+   <Router history={hashHistory}>
+      <Route path='/' component={AuthOrApp}>
+         <IndexRoute component={Dashboard} />
+         <Route path='billingCycles' component={BillingCycle} />
+      </Route>
+      <Redirect from='*' to='/' />
+   </Router>
 )
